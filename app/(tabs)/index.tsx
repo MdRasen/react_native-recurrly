0ca69c125315fca348caa45447465928ae78bad9
Md Al Amin Hossen <aamin.hossen99@gmail.com>
2026-09-04 20:37:33 +0600

    Custom Fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,43 +10,27 @@ export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background">
       <View className="flex-1 p-5">
-        <Text className="text-xl font-bold text-success">
-          Welcome to Nativewind!
-        </Text>
+        <Text className="text-7xl font-sans-extrabold">Home</Text>
 
         <Link
           href="/onboarding"
-          className="mt-4 rounded bg-primary p-4 text-white"
+          className="mt-4 rounded bg-primary p-4 font-sans-bold text-white"
         >
           Go to Onboarding
         </Link>
 
         <Link
           href="/(auth)/sign-in"
-          className="mt-4 rounded bg-primary p-4 text-white"
+          className="mt-4 rounded bg-primary p-4 font-sans-bold text-white"
         >
           Go to Sign In
         </Link>
 
         <Link
           href="/(auth)/sign-up"
-          className="mt-4 rounded bg-primary p-4 text-white"
+          className="mt-4 rounded bg-primary p-4 font-sans-bold text-white"
         >
           Go to Sign Up
-        </Link>
-
-        <Link href="/subscriptions/spotify" className="mt-4">
-          View Spotify Subscription
-        </Link>
-
-        <Link
-          href={{
-            pathname: "/subscriptions/[id]",
-            params: { id: "claude" },
-          }}
-          className="mt-4"
-        >
-          Claude Max Subscription
         </Link>
       </View>
     </SafeAreaView>
